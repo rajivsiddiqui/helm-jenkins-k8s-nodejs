37ad7acf29ab698d54459ea811022d53eb85d8ab
rajivsiddiqui <rajivsiddiqui@gmail.com>
2025-09-04 19:29:06 +0600

    1

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ const APP_NAME = process.env.APP_NAME || "Node Docker Starter";
 
 // Routes
 app.get("/", (_req, res) => {
-  res.send(`🚀 ${APP_NAME} is running. Try /api/health, /api/info, /api/time`);
+  res.send(`Project- ${APP_NAME} is running. Try /api/health, /api/info, /api/time`);
 });
 
 app.get("/api/health", (_req, res) => {
